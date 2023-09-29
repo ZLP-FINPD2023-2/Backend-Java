47dc36441d0ec02f2a7bf166e1c2d31cdc227849
Nejern <5qub6pfp@duck.com>
2023-09-29 21:53:05 +0300

    feat(docker): разделение dev и prod конфигураций

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -10,7 +10,7 @@ rsa_key_size=4096
 data_path="./data/certbot"
 email="nejern@duck.com" # Adding a valid address is strongly recommended
 staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
-docker_compose_file="docker-compose.yml"
+docker_compose_file="docker-compose.prod.yml"
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
