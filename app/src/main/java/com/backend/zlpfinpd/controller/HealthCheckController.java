18f073d11b7e4cfb66e67dfe4e1ef0edf9c6348b
TmP440 <kerambit12323@mail.ru>
2024-03-12 12:43:46 +0300

    add healthcheck handler

--- app/src/main/java/com/backend/zlpfinpd/controller/HealthCheckController.java
+++ app/src/main/java/com/backend/zlpfinpd/controller/HealthCheckController.java
@@ -3,7 +3,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class SimpleHealthCheckController {
+public class HealthCheckController {
 
     @GetMapping("/healthcheck")
     public ResponseEntity<Void> healthCheck() {
