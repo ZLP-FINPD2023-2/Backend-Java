0891c57487abd5e5636fdc61a613526695a03365
dangannn <dn14072004@mail.ru>
2024-03-20 22:41:26 +0300

    fix: required fields + birthday validation

--- app/src/main/java/com/backend/zlpfinpd/model/dto/UserDto.java
+++ app/src/main/java/com/backend/zlpfinpd/model/dto/UserDto.java
@@ -5,6 +5,7 @@ import com.backend.zlpfinpd.model.enums.Gender;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -21,11 +22,15 @@ public class UserDto {
     @NotBlank
     @Email
     private String email;
-    @NotNull
+    @NotBlank
     private String firstName;
-    @NotNull
+    @NotBlank
     private String lastName;
+    @NotBlank
     private String patronymic;
+    @NotNull
     private Gender gender;
+    @NotNull
+    @Past
     private LocalDate birthday;
 }
